c1b17629d7f2d63f6e3665e1738c6ff404cde27b
sihyulater <sihyuwei@berkeley.edu>
2023-03-22 20:42:52 -0700

    added geocoding sample

--- re.py
+++
@@ -1,4 +0,0 @@
-print(r"\t\t")
-string = "SCHIFF, Steven Harvey, a Representative from New Mexico; born in Chicago, Ill.; March 18, 1947"
-string = string.replace("a Representative from New Mexico; born in Chicago, Ill.; March 18, 1947", "born March 18, 1947")
-print(string)
